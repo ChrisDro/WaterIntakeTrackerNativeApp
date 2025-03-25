d5e68a09c5f0120e7b445d351a675f0ccae46bdd
Chris Drozynski <christopherdrozynski@gmail.com>
2025-03-25 09:50:07 -0700

    add initial screens and routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ title: "Bluetooth Device Scanner" }} />
+      <Stack.Screen name="main" options={{ title: "Water Intake Tracker" }} />
+    </Stack>
+  );
 }
---
+++ app/main.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Main() {
+  return (
+    <View>
+      <Text>Screen for Water Intake Tracker</Text>
+    </View>
+  );
+}
